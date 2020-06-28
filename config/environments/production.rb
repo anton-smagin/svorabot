a466fffec7b3b9a8e3caaae824025346b7f12141
Anton Smagin <a.smagin@gettransfer.com>
2020-06-28 16:12:23 +0300

    Add docker-compose

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  routes.default_url_options = { host: 'svora.space', protocol: 'https' }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
