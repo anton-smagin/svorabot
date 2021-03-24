c9d7cb56e764d86b04432bae6099906d57b215ce
Anton Smagin <a.smagin@gettransfer.com>
2021-03-24 14:16:10 +0300

    update ruby and rails

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.6'
+ruby '2.7.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
+gem 'rails', '~> 6.1'
 # Use sqlite3 as the database for Active Record
 gem 'pg'
 # Use Puma as the app server
@@ -25,7 +25,7 @@ gem 'turbolinks', '~> 5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 
-gem 'telegram-bot', '~> 0.14.4'
+gem 'telegram-bot', '~> 0.15.3'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -41,17 +41,14 @@ gem 'telegram-bot', '~> 0.14.4'
 
 gem 'acts_as_paranoid'
 
-group :pry do
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'awesome_print'
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry'
   gem 'pry-byebug'
   gem 'pry-doc'
   gem 'pry-rails'
-end
-
-group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,10 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups(
-  assets: %i[development test],
-  pry:    %i[development test],
-))
-# load pry for production console
-Bundler.require(:pry) if defined?(Rails::Console)
+Bundler.require(*Rails.groups)
 
 module TelegramBotApp
   class Application < Rails::Application
@@ -18,9 +13,12 @@ module TelegramBotApp
 
     config.i18n.default_locale = :ru
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
