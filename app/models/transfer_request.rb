93f51c31c0fbfdbfa78cf16807d7342ded5c3106
Anton Smagin <a.smagin@gettransfer.com>
2021-07-20 00:04:15 +0300

    Change transfer request count

--- app/models/transfer_request.rb
+++ app/models/transfer_request.rb
@@ -17,12 +17,12 @@ class TransferRequest < ApplicationRecord # :nodoc:
     },
     'transfer_from_svora_01_08_17_00' => {
       price: 400,
-      count: 22,
+      count: 23,
       type: :return
     },
     'transfer_from_svora_01_08_23_00' => {
       price: 400,
-      count: 36,
+      count: 35,
       type: :return
     },
     'transfer_from_moscow_28_07_01_08' => {
